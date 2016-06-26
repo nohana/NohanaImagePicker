f783e06002acae9d57115b2e6a6ca45a0ad19b75
haranicle <haranicle@gmail.com>
2016-06-26 16:08:37 +0900

    fix copyright

--- NohanaImagePicker/ColorConfig.swift
+++ NohanaImagePicker/ColorConfig.swift
@@ -1,10 +1,18 @@
-//
-//  ColorConfig.swift
-//  NohanaImagePicker
-//
-//  Created by kazushi.hara on 2016/03/23.
-//  Copyright © 2016年 nohana. All rights reserved.
-//
+/*
+ * Copyright (C) 2016 nohana, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 public struct ColorConfig {
     public static var backgroundColor = UIColor.whiteColor()
