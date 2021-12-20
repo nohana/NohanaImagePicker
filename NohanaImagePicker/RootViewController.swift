c4dba89202e6f066e73a9c6a3f663f4fb073a1a8
atsushi.yoshimoto <atsushi.yoshimoto@nohana.co.jp>
2021-12-20 13:01:25 +0900

    Implemented init.

--- NohanaImagePicker/RootViewController.swift
+++ NohanaImagePicker/RootViewController.swift
@@ -17,6 +17,17 @@
 import UIKit
 
 class RootViewController: UIViewController {
+    
+    private let nohanaImagePickerController: NohanaImagePickerController
+    
+    init?(coder: NSCoder, nohanaImagePickerController: NohanaImagePickerController) {
+        self.nohanaImagePickerController = nohanaImagePickerController
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
