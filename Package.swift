70b83a6e1093755eee833b7dd7b86a684f054bf7
NoodleKim <gibongkim80@gmail.com>
2022-12-21 09:50:40 +0900

    Testing for an unit test issue.

--- Package.swift
+++ Package.swift
@@ -23,10 +23,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Demo",
-            dependencies: [],
-            path: "Demo"),
-        .target(
             name: "NohanaImagePicker",
             dependencies: [],
             path: "NohanaImagePicker"
